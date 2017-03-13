263116a62b0bc9f86186492586ed890e5d0fecc7
Ale <alexcoppe@gmail.com>
2017-03-13 22:44:04 +0100

    var2vcf_paired.pl added to launch_vardict.sh

--- vardict/launch_vardict.sh
+++ vardict/launch_vardict.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-AVAILABLE_COMMANDS=("VarDict" "teststrandbias.R" "var2vcf_valid.pl" "testsomatic.R")
+AVAILABLE_COMMANDS=("VarDict" "teststrandbias.R" "var2vcf_valid.pl" "testsomatic.R" "var2vcf_paired.pl")
 
 
 if [[ " ${AVAILABLE_COMMANDS[@]} " =~ " $1 " ]]; then
