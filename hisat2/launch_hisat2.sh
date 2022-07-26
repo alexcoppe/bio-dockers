80c110e33dd013373748bb2d7fbe474638e10c3e
Ale <alexcoppe@gmail.com>
2022-07-26 16:53:06 +0200

    Cleaned hisat2

--- hisat2/launch_hisat2.sh
+++ hisat2/launch_hisat2.sh
@@ -2,4 +2,4 @@
 
 AVAILABLE_COMMANDS=("hisat2", "hisat2-align-s", "hisat2-align-l", "hisat2_extract_splice_sites.py", "hisat2_extract_exons.py", "hisat2-build", "hisat2-build-l", "hisat2-build-new", "hisat2-build-s", "hisat2-inspect", "hisat2-inspect-l", "hisat2-inspect-s", "hisat2-repeat", "hisat2_extract_snps_haplotypes_UCSC.py", "hisat2_extract_snps_haplotypes_VCF.py", "hisat2_read_statistics.py", "hisat2_simulate_reads.py")
 
-/hisat2/$1 ${*:2:$#}
+/hisat/$1 ${*:2:$#}
